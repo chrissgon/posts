31adfa88abd732144fdd4de67b83b6e773bc0e75
Christopher Gonçalves <chrissgon.dev@gmail.com>
2023-06-14 19:22:37 -0300

    fix: :bug: add dist folder

--- bootstrap_vite/vite.config.js
+++ bootstrap_vite/vite.config.js
@@ -1,14 +1,20 @@
+// vite.config.js
+
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
   build: {
+    // Define lib configuration
     lib: {
+      // Set the main file
       entry: resolve(__dirname, "main.js"),
+      // Set the lib name
       name: "bootstrap",
     },
     rollupOptions: {
       output: {
+        // Output JS and CSS filenames
         entryFileNames: "bootstrap.js",
         assetFileNames: "bootstrap.css",
       },
